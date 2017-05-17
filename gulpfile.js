0ffe11bbcccd843bdf746cba28543c53c305c9f0
Rajko Tomic <phxtetsu@gmail.com>
2017-05-17 15:28:40 +0200

    npm-Gulp-Bootstrap demo

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('default', function(){
+  console.log('first task');
+});
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+})
+
+gulp.task('watch', ['browserSync'], function() {
+    gulp.watch('app/index.html', browserSync.reload);
+    gulp.watch('app/css/*.css', browserSync.reload);
+    gulp.watch('app/js/*.js', browserSync.reload);
+})
